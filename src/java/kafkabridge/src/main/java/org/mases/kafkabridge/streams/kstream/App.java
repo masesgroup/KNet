645fe01747267b8bebaee99c0dbc2fd115d391f6
masesdevelopers <94312179+masesdevelopers@users.noreply.github.com>
2021-12-13 20:52:00 +0100

    Added java project, update source structure

---
+++ src/java/kafkabridge/src/main/java/org/mases/kafkabridge/streams/kstream/App.java
@@ -0,0 +1,13 @@
+package org.mases.kafkabridge.streams.kstream;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ src/java/kafkabridge/src/test/java/org/mases/kafkabridge/streams/kstream/AppTest.java
@@ -0,0 +1,20 @@
+package org.mases.kafkabridge.streams.kstream;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
