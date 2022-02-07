53041b6e3f09a005fbc2f7ff7ac3cc7bb0f36237
masesdevelopers <94312179+masesdevelopers@users.noreply.github.com>
2022-02-07 23:12:04 +0100

    #19: initial setup of Kafka Connect API

---
+++ src/net/KafkaBridge/ClientSide/BridgedClasses/Connect/Data/ConnectSchema.cs
@@ -0,0 +1,68 @@
+﻿/*
+*  Copyright 2022 MASES s.r.l.
+*
+*  Licensed under the Apache License, Version 2.0 (the "License");
+*  you may not use this file except in compliance with the License.
+*  You may obtain a copy of the License at
+*
+*  http://www.apache.org/licenses/LICENSE-2.0
+*
+*  Unless required by applicable law or agreed to in writing, software
+*  distributed under the License is distributed on an "AS IS" BASIS,
+*  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+*  See the License for the specific language governing permissions and
+*  limitations under the License.
+*
+*  Refer to LICENSE for more information.
+*/
+
+using MASES.JCOBridge.C2JBridge;
+using MASES.KafkaBridge.Java.Util;
+using System;
+
+namespace MASES.KafkaBridge.Connect.Data
+{
+    public class ConnectSchema : Schema
+    {
+        public override string ClassName => "org.apache.kafka.connect.data.ConnectSchema";
+
+        [Obsolete("This is not public in Apache Kafka API")]
+        public ConnectSchema()
+        {
+        }
+
+        public ConnectSchema(Type type, bool optional, Java.Lang.Object defaultValue, string name, int version, string doc, Map<string, string> parameters, List<Field> fields, Schema keySchema, Schema valueSchema)
+            : base(type, optional, defaultValue, name, version, doc, parameters, fields, keySchema, valueSchema)
+        {
+        }
+
+        public ConnectSchema(Type type, bool optional, Java.Lang.Object defaultValue, string name, int version, string doc)
+            :this(type, optional, defaultValue, name, version, doc, null, null, null, null)
+        {
+        }
+
+        public ConnectSchema(Type type)
+            : base(type)
+        {
+        }
+
+        public static void ValidateValue(Schema schema, object value)
+        {
+            SExecute<SchemaBuilder>("validateValue", schema, value);
+        }
+
+        public static void ValidateValue(string name, Schema schema, object value)
+        {
+            SExecute<SchemaBuilder>("validateValue",name, schema, value);
+        }
+
+        //public static Type schemaType(Class klass)
+        //{
+        //}
+
+        public void ValidateValue(object value)
+        {
+            IExecute("validateValue", value);
+        }
+    }
+}
