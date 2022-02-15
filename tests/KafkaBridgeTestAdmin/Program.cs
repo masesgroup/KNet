d160c3495c1b83fb8e4adf2bc0a8e16676adaa23
masesdevelopers <94312179+masesdevelopers@users.noreply.github.com>
2022-02-15 13:14:46 +0100

    #4: added Admin basic test program

---
+++ tests/KafkaBridgeTestAdmin/Program.cs
@@ -0,0 +1,99 @@
+﻿/*
+ *  MIT License
+ *
+ *  Copyright (c) 2022 MASES s.r.l.
+ *
+ *  Permission is hereby granted, free of charge, to any person obtaining a copy
+ *  of this software and associated documentation files (the "Software"), to deal
+ *  in the Software without restriction, including without limitation the rights
+ *  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ *  copies of the Software, and to permit persons to whom the Software is
+ *  furnished to do so, subject to the following conditions:
+ *
+ *  The above copyright notice and this permission notice shall be included in all
+ *  copies or substantial portions of the Software.
+ *
+ *  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ *  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ *  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ *  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ *  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ *  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ *  SOFTWARE.
+ */
+
+using MASES.KafkaBridge;
+using MASES.KafkaBridge.Clients.Admin;
+using MASES.KafkaBridge.Common.Config;
+using MASES.KafkaBridge.Java.Util;
+using System;
+using System.Threading;
+
+namespace MASES.KafkaBridgeTestAdmin
+{
+    class Program
+    {
+        static bool useSerdes = true;
+        static bool useCallback = true;
+
+        const string theServer = "localhost:9092";
+        const string theTopic = "myTopic";
+
+        static string serverToUse = theServer;
+        static string topicToUse = theTopic;
+        static ManualResetEvent resetEvent = new ManualResetEvent(false);
+
+        static void Main(string[] args)
+        {
+            var appArgs = KafkaBridgeCore.ApplicationArgs;
+
+            if (appArgs.Length != 0)
+            {
+                serverToUse = args[0];
+            }
+
+            Properties props = new Properties();
+            props.Put(AdminClientConfig.BOOTSTRAP_SERVERS_CONFIG, serverToUse);
+
+            using (var admin = KafkaAdminClient.Create(props))
+            {
+                createTopic(admin);
+
+
+            }
+        }
+
+        static void createTopic(IAdmin admin)
+        {
+            try
+            {
+                string topicName = topicToUse;
+                int partitions = 1;
+                short replicationFactor = 1;
+
+                var topic = new NewTopic(topicName, partitions, replicationFactor);
+                var map = Collections.SingletonMap(TopicConfig.CLEANUP_POLICY_CONFIG, TopicConfig.CLEANUP_POLICY_COMPACT);
+                topic.Configs(map);
+                var coll = Collections.Singleton(topic);
+
+                // Create a compacted topic
+                CreateTopicsResult result = admin.CreateTopics(coll);
+
+                // Call values() to get the result for a specific topic
+                var future = result.Values.Get(topicName);
+
+                // Call get() to block until the topic creation is complete or has failed
+                // if creation failed the ExecutionException wraps the underlying cause.
+                future.Get();
+            }
+            catch (KafkaBridge.Java.Util.Concurrent.ExecutionException ex)
+            {
+                Console.WriteLine(ex.InnerException.Message);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+        }
+    }
+}
