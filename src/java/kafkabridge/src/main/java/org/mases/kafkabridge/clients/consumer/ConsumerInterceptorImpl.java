32e954812180519705d942a17a0e3e842b464261
masesdevelopers <94312179+masesdevelopers@users.noreply.github.com>
2022-03-15 04:18:22 +0100

    #1: improvements of Consumer APIs

---
+++ src/java/kafkabridge/src/main/java/org/mases/kafkabridge/clients/consumer/ConsumerInterceptorImpl.java
@@ -0,0 +1,59 @@
+/*
+ *  MIT License
+ *
+ *  Copyright (c) 2022 MASES s.r.l.
+ *
+ *  Permission is hereby granted, free of charge, to any person obtaining a copy
+ *  of this software and associated documentation files (the "Software"), to deal
+ *  in the Software without restriction, including without limitation the rights
+ *  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ *  copies of the Software, and to permit persons to whom the Software is
+ *  furnished to do so, subject to the following conditions:
+ *
+ *  The above copyright notice and this permission notice shall be included in all
+ *  copies or substantial portions of the Software.
+ *
+ *  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ *  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ *  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ *  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ *  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ *  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ *  SOFTWARE.
+ */
+
+package org.mases.kafkabridge.clients.consumer;
+
+import org.apache.kafka.clients.consumer.ConsumerInterceptor;
+import org.apache.kafka.clients.consumer.ConsumerRecords;
+import org.mases.jcobridge.*;
+
+import java.util.Map;
+
+public final class ConsumerInterceptorImpl extends JCListener implements ConsumerInterceptor {
+    public ConsumerInterceptorImpl(String key) throws JCNativeException {
+        super(key);
+    }
+
+    @Override
+    public void configure(Map configs) {
+        raiseEvent("configure", configs);
+    }
+
+    @Override
+    public ConsumerRecords onConsume(ConsumerRecords records) {
+        raiseEvent("onConsume", records);
+        Object retVal = getReturnData();
+        return (ConsumerRecords) retVal;
+    }
+
+    @Override
+    public void onCommit(Map offsets) {
+        raiseEvent("onCommit", offsets);
+    }
+
+    @Override
+    public void close() {
+        raiseEvent("close");
+    }
+}
---
+++ src/java/kafkabridge/src/test/java/org/mases/kafkabridge/clients/consumer/ConsumerInterceptorImplTest.java
@@ -0,0 +1,18 @@
+package org.mases.kafkabridge.clients.consumer;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertTrue;
+
+/**
+ * Unit test for simple App.
+ */
+public class ConsumerInterceptorImplTest {
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue() {
+        assertTrue(true);
+    }
+}
