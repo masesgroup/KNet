f413a7b4d3a50f911f76e00fe7392193040e3d2b
masesdevelopers <94312179+masesdevelopers@users.noreply.github.com>
2022-04-22 00:24:31 +0200

    #53: Fix file

--- src/net/KNet/ClientSide/BridgedClasses/Common/Metrics/MetricsReporter.cs
+++ src/net/KNet/ClientSide/BridgedClasses/Common/Metrics/MetricsReporter.cs
@@ -16,61 +16,150 @@
 *  Refer to LICENSE for more information.
 */
 
+using Java.Util;
 using MASES.JCOBridge.C2JBridge;
 using MASES.JCOBridge.C2JBridge.JVMInterop;
 using System;
 
-namespace MASES.KNet.Clients.Producer
+namespace MASES.KNet.Common.Metrics
 {
     /// <summary>
-    /// Listener for Kafka Callback. Extends <see cref="IJVMBridgeBase"/>
+    /// Listener for Kafka MetricsReporter. Extends <see cref="IJVMBridgeBase"/>
     /// </summary>
-    public interface ICallback : IJVMBridgeBase
+    public interface IMetricsReporter : IJVMBridgeBase
     {
-        /// <summary>
-        /// Executes the Callback action in the CLR
-        /// </summary>
-        /// <param name="metadata">The <see cref="RecordMetadata"/> object</param>
-        /// <param name="exception">The <see cref="JVMBridgeException"/> object</param>
-        void OnCompletion(RecordMetadata metadata, JVMBridgeException exception);
+        void Init(List<KafkaMetric> metrics);
+
+        void MetricChange(KafkaMetric metric);
+
+        void MetricRemoval(KafkaMetric metric);
+
+        void Close();
+
+        Set<string> ReconfigurableConfigs();
+
+        void Configure(Map<string, object> configs);
+
+        void Reconfigure(Map<string, object> configs);
+
+        void ValidateReconfiguration(Map<string, object> configs);
+
+        void ContextChange(MetricsContext metricsContext);
     }
 
     /// <summary>
-    /// Listener for Kafka Callback. Extends <see cref="JVMBridgeListener"/>, implements <see cref="ICallback"/>
+    /// Listener for Kafka MetricsReporter. Extends <see cref="JVMBridgeListener"/>, implements <see cref="IMetricsReporter"/>
     /// </summary>
     /// <remarks>Dispose the object to avoid a resource leak, the object contains a reference to the corresponding JVM object</remarks>
-    public class Callback : JVMBridgeListener, ICallback
+    public abstract class MetricsReporter : JVMBridgeListener, IMetricsReporter
     {
         /// <inheritdoc cref="JVMBridgeListener.ClassName"/>
-        public sealed override string ClassName => "org.mases.knet.clients.producer.CallbackImpl";
-
-        readonly Action<RecordMetadata, JVMBridgeException> executionFunction = null;
+        public sealed override string ClassName => "org.mases.knet.clients.producer.MetricsReporterImpl";
         /// <summary>
-        /// The <see cref="Action{RecordMetadata, JVMBridgeException}"/> to be executed
+        /// Initialize a new instance of <see cref="MetricsReporter"/>
         /// </summary>
-        public virtual Action<RecordMetadata, JVMBridgeException> OnOnCompletion { get { return executionFunction; } }
-        /// <summary>
-        /// Initialize a new instance of <see cref="Callback"/>
-        /// </summary>
-        /// <param name="action">The <see cref="Action{RecordMetadata, JVMBridgeException}"/> to be executed</param>
-        public Callback(Action<RecordMetadata, JVMBridgeException> action = null)
+        public MetricsReporter()
         {
-            if (action != null) executionFunction = action;
-            else executionFunction = OnCompletion;
-
-            AddEventHandler("onCompletion", new EventHandler<CLRListenerEventArgs<CLREventData<RecordMetadata>>>(EventHandler));
+            AddEventHandler("init", new EventHandler<CLRListenerEventArgs<CLREventData<List<KafkaMetric>>>>(InitEventHandler));
+            AddEventHandler("metricChange", new EventHandler<CLRListenerEventArgs<CLREventData<KafkaMetric>>>(MetricChangeEventHandler));
+            AddEventHandler("metricRemoval", new EventHandler<CLRListenerEventArgs<CLREventData<KafkaMetric>>>(MetricRemovalEventHandler));
+            AddEventHandler("close", new EventHandler<CLRListenerEventArgs<CLREventData>>(CloseEventHandler));
+            AddEventHandler("reconfigurableConfigs", new EventHandler<CLRListenerEventArgs<CLREventData>>(ReconfigurableConfigsEventHandler));
+            AddEventHandler("configure", new EventHandler<CLRListenerEventArgs<CLREventData<Map<string, object>>>>(ConfigureEventHandler));
+            AddEventHandler("reconfigure", new EventHandler<CLRListenerEventArgs<CLREventData<Map<string, object>>>>(ReconfigureEventHandler));
+            AddEventHandler("validateReconfiguration", new EventHandler<CLRListenerEventArgs<CLREventData<Map<string, object>>>>(ValidateReconfigurationEventHandler));
+            AddEventHandler("contextChange", new EventHandler<CLRListenerEventArgs<CLREventData<MetricsContext>>>(ContextChangeEventHandler));
         }
 
-        void EventHandler(object sender, CLRListenerEventArgs<CLREventData<RecordMetadata>> data)
+        void InitEventHandler(object sender, CLRListenerEventArgs<CLREventData<List<KafkaMetric>>> data)
         {
-            var exception = data.EventData.ExtraData.Get(0) as IJavaObject;
-            OnOnCompletion(data.EventData.TypedEventData, JVMBridgeException.New(exception));
+            Init(data.EventData.TypedEventData);
         }
-        /// <summary>
-        /// Executes the Callback action in the CLR
-        /// </summary>
-        /// <param name="metadata">The <see cref="RecordMetadata"/> object</param>
-        /// <param name="exception">The <see cref="JVMBridgeException"/> object</param>
-        public virtual void OnCompletion(RecordMetadata metadata, JVMBridgeException exception) { }
+
+        void MetricChangeEventHandler(object sender, CLRListenerEventArgs<CLREventData<KafkaMetric>> data)
+        {
+            MetricChange(data.EventData.TypedEventData);
+        }
+
+        void MetricRemovalEventHandler(object sender, CLRListenerEventArgs<CLREventData<KafkaMetric>> data)
+        {
+            MetricRemoval(data.EventData.TypedEventData);
+        }
+
+        void CloseEventHandler(object sender, CLRListenerEventArgs<CLREventData> data)
+        {
+            Close();
+        }
+
+        void ReconfigurableConfigsEventHandler(object sender, CLRListenerEventArgs<CLREventData> data)
+        {
+            var result = ReconfigurableConfigs();
+            data.SetReturnValue(result);
+        }
+
+        void ConfigureEventHandler(object sender, CLRListenerEventArgs<CLREventData<Map<string, object>>> data)
+        {
+            Configure(data.EventData.TypedEventData);
+        }
+
+        void ReconfigureEventHandler(object sender, CLRListenerEventArgs<CLREventData<Map<string, object>>> data)
+        {
+            Reconfigure(data.EventData.TypedEventData);
+        }
+
+        void ValidateReconfigurationEventHandler(object sender, CLRListenerEventArgs<CLREventData<Map<string, object>>> data)
+        {
+            ValidateReconfiguration(data.EventData.TypedEventData);
+        }
+
+        void ContextChangeEventHandler(object sender, CLRListenerEventArgs<CLREventData<MetricsContext>> data)
+        {
+            ContextChange(data.EventData.TypedEventData);
+        }
+
+        public virtual void Init(List<KafkaMetric> metrics)
+        {
+
+        }
+
+        public virtual void MetricChange(KafkaMetric metric)
+        {
+
+        }
+
+        public virtual void MetricRemoval(KafkaMetric metric)
+        {
+
+        }
+
+        public virtual void Close()
+        {
+
+        }
+
+        public virtual Set<string> ReconfigurableConfigs()
+        {
+            return Wraps<Set<string>>(Collections.DynClazz.emptySet() as IJavaObject);
+        }
+
+        public virtual void Configure(Map<string, object> configs)
+        {
+
+        }
+
+        public virtual void Reconfigure(Map<string, object> configs)
+        {
+
+        }
+
+        public virtual void ValidateReconfiguration(Map<string, object> configs)
+        {
+
+        }
+
+        public virtual void ContextChange(MetricsContext metricsContext)
+        {
+
+        }
     }
 }
