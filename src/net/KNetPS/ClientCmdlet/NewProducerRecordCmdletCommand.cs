052bb968da0aa66f3ba6979bf67fd049835cf74f
masesdevelopers <94312179+masesdevelopers@users.noreply.github.com>
2022-10-30 19:54:06 +0100

    #100: added first Producer methods

---
+++ src/net/KNetPS/ClientCmdlet/NewProducerRecordCmdletCommand.cs
@@ -0,0 +1,103 @@
+﻿/*
+*  Copyright 2022 MASES s.r.l.
+*
+*  Licensed under the Apache License, Version 2.0 (the "License");
+*  you may not use this file except in compliance with the License.
+*  You may obtain a copy of the License at
+*
+*  http://www.apache.org/licenses/LICENSE-2.0
+*
+*  Unless required by applicable law or agreed to in writing, software
+*  distributed under the License is distributed on an "AS IS" BASIS,
+*  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+*  See the License for the specific language governing permissions and
+*  limitations under the License.
+*
+*  Refer to LICENSE for more information.
+*/
+
+using MASES.KNet.Clients.Producer;
+using MASES.KNetPS.Cmdlet;
+using System;
+using System.Management.Automation;
+
+namespace MASES.KNetPS.ClientCmdlet
+{
+    [Cmdlet(VerbsCommon.New, "ProducerRecord")]
+    [OutputType(typeof(ProducerRecord<,>))]
+    public class NewProducerRecordCmdletCommand : KNetPSCmdlet
+    {
+        [Parameter(
+            Mandatory = true,
+            Position = 0,
+            ValueFromPipeline = true,
+            ValueFromPipelineByPropertyName = true,
+            HelpMessage = "The dotnet class of the key")]
+        public string KeyClass { get; set; }
+
+        [Parameter(
+            Mandatory = true,
+            Position = 1,
+            ValueFromPipeline = true,
+            ValueFromPipelineByPropertyName = true,
+            HelpMessage = "The key")]
+        public object Key { get; set; }
+
+        [Parameter(
+            Mandatory = true,
+            Position = 2,
+            ValueFromPipeline = true,
+            ValueFromPipelineByPropertyName = true,
+            HelpMessage = "The dotnet class of the value")]
+        public string ValueClass { get; set; }
+
+        [Parameter(
+            Mandatory = true,
+            Position = 3,
+            ValueFromPipeline = true,
+            ValueFromPipelineByPropertyName = true,
+            HelpMessage = "The value")]
+        public object Value { get; set; }
+
+        [Parameter(
+            Mandatory = true,
+            Position = 4,
+            ValueFromPipeline = true,
+            ValueFromPipelineByPropertyName = true,
+            HelpMessage = "The topic name")]
+        public string Topic { get; set; }
+
+        // This method gets called once for each cmdlet in the pipeline when the pipeline starts executing
+        protected override void BeginProcessing()
+        {
+            WriteVerbose("Begin ProducerRecord!");
+        }
+
+        // This method will be called for each input received from the pipeline to this cmdlet; if no input is received, this method is not called
+        protected override void ProcessCommand()
+        {
+            System.Type keyType = System.Type.GetType(KeyClass);
+            System.Type valueType = System.Type.GetType(ValueClass);
+
+            object key = Key;
+            if (key is PSObject pskey) key = pskey.BaseObject;
+
+            object value = Value;
+            if (value is PSObject psvalue) value = psvalue.BaseObject;
+
+            var recordType = typeof(ProducerRecord<,>).MakeGenericType(keyType, valueType);
+
+            var record = Activator.CreateInstance(recordType, Topic, key, value);
+
+            WriteObject(record);
+        }
+
+        // This method will be called once at the end of pipeline execution; if no input is received, this method is not called
+        protected override void EndProcessing()
+        {
+            WriteVerbose("End KNetProducer!");
+        }
+
+
+    }
+}
