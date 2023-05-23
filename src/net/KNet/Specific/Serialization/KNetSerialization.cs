ef2c0fdba14ea72298a7c73d39f0ac961065d3ce
masesdevelopers <94312179+masesdevelopers@users.noreply.github.com>
2023-05-23 12:45:25 +0200

    #175: moved to Java SerDes due to error in C# compilation within container

--- src/net/KNet/Specific/Serialization/KNetSerialization.cs
+++ src/net/KNet/Specific/Serialization/KNetSerialization.cs
@@ -113,19 +113,28 @@ namespace MASES.KNet.Serialization
             return _FloatSerializer.Serialize(topic, data);
         }
 
+        static readonly Serializer<int> _IntSerializer = new IntegerSerializer();
         public static byte[] SerializeInt(string topic, int data)
         {
-            return new byte[] { (byte)(data >>> 24), (byte)(data >>> 16), (byte)(data >>> 8), ((byte)data) };
+            return _IntSerializer.Serialize(topic, data);
+            // the following generates an error in container
+            //return new byte[] { (byte)(data >>> 24), (byte)(data >>> 16), (byte)(data >>> 8), ((byte)data) };
         }
 
+        static readonly Serializer<long> _LongSerializer = new LongSerializer();
         public static byte[] SerializeLong(string topic, long data)
         {
-            return new byte[] { (byte)((int)(data >>> 56)), (byte)((int)(data >>> 48)), (byte)((int)(data >>> 40)), (byte)((int)(data >>> 32)), (byte)((int)(data >>> 24)), (byte)((int)(data >>> 16)), (byte)((int)(data >>> 8)), ((byte)data) };
+            return _LongSerializer.Serialize(topic, data);
+            // the following generates an error in container
+            //return new byte[] { (byte)((int)(data >>> 56)), (byte)((int)(data >>> 48)), (byte)((int)(data >>> 40)), (byte)((int)(data >>> 32)), (byte)((int)(data >>> 24)), (byte)((int)(data >>> 16)), (byte)((int)(data >>> 8)), ((byte)data) };
         }
 
+        static readonly Serializer<short> _ShortSerializer = new ShortSerializer();
         public static byte[] SerializeShort(string topic, short data)
         {
-            return new byte[] { (byte)(data >>> 8), ((byte)data) };
+            return _ShortSerializer.Serialize(topic, data);
+            // the following generates an error in container
+            //return new byte[] { (byte)(data >>> 8), ((byte)data) };
         }
 
         public static byte[] SerializeString(string topic, string data)
@@ -172,88 +181,97 @@ namespace MASES.KNet.Serialization
             return _FloatDeserializer.Deserialize(topic, data);
         }
 
+        static readonly Deserializer<int> _IntDeserializer = new IntegerDeserializer();
         public static int DeserializeInt(string topic, byte[] data)
         {
-            if (data == null)
-            {
-                return default;
-            }
-            else if (data.Length != 4)
-            {
-                JVMBridgeException<SerializationException>.ThrowNew("Size of data received by DeserializeInt is not 4");
-                throw new SerializationException();
-            }
-            else
-            {
-                int value = 0;
-                byte[] var4 = data;
-                int var5 = data.Length;
+            return _IntDeserializer.Deserialize(topic, data);
 
-                for (int var6 = 0; var6 < var5; ++var6)
-                {
-                    byte b = var4[var6];
-                    value <<= 8;
-                    value |= b & 255;
-                }
+            //if (data == null)
+            //{
+            //    return default;
+            //}
+            //else if (data.Length != 4)
+            //{
+            //    JVMBridgeException<SerializationException>.ThrowNew("Size of data received by DeserializeInt is not 4");
+            //    throw new SerializationException();
+            //}
+            //else
+            //{
+            //    int value = 0;
+            //    byte[] var4 = data;
+            //    int var5 = data.Length;
 
-                return value;
-            }
+            //    for (int var6 = 0; var6 < var5; ++var6)
+            //    {
+            //        byte b = var4[var6];
+            //        value <<= 8;
+            //        value |= b & 255;
+            //    }
+
+            //    return value;
+            //}
         }
 
+        static readonly Deserializer<long> _LongDeserializer = new LongDeserializer();
         public static long DeserializeLong(string topic, byte[] data)
         {
-            if (data == null)
-            {
-                return default;
-            }
-            else if (data.Length != 8)
-            {
-                JVMBridgeException<SerializationException>.ThrowNew("Size of data received by DeserializeLong is not 8");
-                throw new SerializationException();
-            }
-            else
-            {
-                long value = 0L;
-                byte[] var5 = data;
-                int var6 = data.Length;
+            return _LongDeserializer.Deserialize(topic, data);
 
-                for (int var7 = 0; var7 < var6; ++var7)
-                {
-                    byte b = var5[var7];
-                    value <<= 8;
-                    value |= (long)(b & 255);
-                }
+            //if (data == null)
+            //{
+            //    return default;
+            //}
+            //else if (data.Length != 8)
+            //{
+            //    JVMBridgeException<SerializationException>.ThrowNew("Size of data received by DeserializeLong is not 8");
+            //    throw new SerializationException();
+            //}
+            //else
+            //{
+            //    long value = 0L;
+            //    byte[] var5 = data;
+            //    int var6 = data.Length;
 
-                return value;
-            }
+            //    for (int var7 = 0; var7 < var6; ++var7)
+            //    {
+            //        byte b = var5[var7];
+            //        value <<= 8;
+            //        value |= (long)(b & 255);
+            //    }
+
+            //    return value;
+            //}
         }
 
+        static readonly Deserializer<short> _ShortDeserializer = new ShortDeserializer();
         public static short DeserializeShort(string topic, byte[] data)
         {
-            if (data == null)
-            {
-                return default;
-            }
-            else if (data.Length != 2)
-            {
-                JVMBridgeException<SerializationException>.ThrowNew("Size of data received by DeserializeShort is not 2");
-                throw new SerializationException();
-            }
-            else
-            {
-                short value = 0;
-                byte[] var4 = data;
-                int var5 = data.Length;
+            return _ShortDeserializer.Deserialize(topic, data);
 
-                for (int var6 = 0; var6 < var5; ++var6)
-                {
-                    byte b = var4[var6];
-                    value = (short)(value << 8);
-                    value = (short)(value | (b & 255));
-                }
+            //if (data == null)
+            //{
+            //    return default;
+            //}
+            //else if (data.Length != 2)
+            //{
+            //    JVMBridgeException<SerializationException>.ThrowNew("Size of data received by DeserializeShort is not 2");
+            //    throw new SerializationException();
+            //}
+            //else
+            //{
+            //    short value = 0;
+            //    byte[] var4 = data;
+            //    int var5 = data.Length;
 
-                return value;
-            }
+            //    for (int var6 = 0; var6 < var5; ++var6)
+            //    {
+            //        byte b = var4[var6];
+            //        value = (short)(value << 8);
+            //        value = (short)(value | (b & 255));
+            //    }
+
+            //    return value;
+            //}
         }
 
         public static string DeserializeString(string topic, byte[] data)
