984500f33a2df1340c8002d4a01f703deedbc972
masesdevelopers <94312179+masesdevelopers@users.noreply.github.com>
2023-10-05 15:29:30 +0200

    Improved DeserializeWithHeaders when the input is null

--- src/net/KNet.Serialization.MessagePack/MessagePackSerDes.cs
+++ src/net/KNet.Serialization.MessagePack/MessagePackSerDes.cs
@@ -32,14 +32,25 @@ namespace MASES.KNet.Serialization.MessagePack
         /// The extension uses <see cref="Headers"/>
         /// </summary>
         public override bool UseHeaders => true;
+        /// <inheritdoc cref="KNetSerDes{T}.Serialize(string, T)"/>
+        public override byte[] Serialize(string topic, T data)
+        {
+            return SerializeWithHeaders(topic, null, data);
+        }
         /// <inheritdoc cref="KNetSerDes{T}.SerializeWithHeaders(string, Headers, T)"/>
         public override byte[] SerializeWithHeaders(string topic, Headers headers, T data)
         {
             return MessagePackSerializer.Serialize(data);
         }
+        /// <inheritdoc cref="KNetSerDes{T}.Deserialize(string, byte[])"/>
+        public override T Deserialize(string topic, byte[] data)
+        {
+            return DeserializeWithHeaders(topic, null, data);
+        }
         /// <inheritdoc cref="KNetSerDes{T}.DeserializeWithHeaders(string, Headers, byte[])"/>
         public override T DeserializeWithHeaders(string topic, Headers headers, byte[] data)
         {
+            if (data == null) return default;
             using (MemoryStream stream = new MemoryStream(data))
             {
                 return MessagePackSerializer.Deserialize<T>(stream);
