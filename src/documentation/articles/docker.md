3b49c260e0da0738b8597347d5d3c78df2d92c1c
MASES Public Developers Team <94312179+masesdevelopers@users.noreply.github.com>
2024-02-02 00:20:04 +0100

    Added interactive and script based mode into KNetCLI (#407)
    * Added interactive and script based mode into KNetCLI
    
    * Update documentation

---
+++ src/documentation/articles/docker.md
@@ -0,0 +1,10 @@
+---
+title: Docker images of .NET suite for Apache Kafka
+_description: Describes the Docker images of .NET suite for Apache Kafka
+---
+
+# KNet: Docker images
+
+.NET suite for Apache Kafka comes with some ready made Docker images:
+- KNet CLI: see [KNet CLI usage](usageCLI.md)
+- KNet Connect: see [KNet Connect usage](usageConnect.md)
--- src/net/KNetCLI/KNetCLICore.cs
+++ src/net/KNetCLI/KNetCLICore.cs
@@ -16,6 +16,8 @@
 *  Refer to LICENSE for more information.
 */
 
+using MASES.CLIParser;
+using MASES.JNet;
 using MASES.KNet;
 using System;
 using System.Collections.Generic;
@@ -29,29 +31,154 @@ namespace MASES.KNetCLI
     public class KNetCLICore<T> : KNetCore<T>
         where T : KNetCLICore<T>
     {
+        class CLIParam
+        {
+            // ReflectorArgs
+            public static string[] Interactive = new string[] { "Interactive", "i" };
+            public static string[] NoLogo = new string[] { "NoLogo", "nl" };
+            public static string[] Script = new string[] { "Script", "s" };
+            public static string[] JarList = new string[] { "JarList", "jl" };
+            public static string[] NamespaceList = new string[] { "NamespaceList", "nl" };
+            public static string[] ImportList = new string[] { "ImportList", "il" };
+        }
+
+        static bool _Interactive;
+        public static bool Interactive => _Interactive;
+
+        static bool _NoLogo;
+        public static bool NoLogo => _NoLogo;
+
+        static string _Script;
+        public static string Script => _Script;
+
+        static IEnumerable<string> _JarList;
+        public static IEnumerable<string> JarList => _JarList;
+
+        static IEnumerable<string> _NamespaceList;
+        public static IEnumerable<string> NamespaceList => _NamespaceList;
+
+        static IEnumerable<string> _ImportList;
+        public static IEnumerable<string> ImportList => _ImportList;
+
+        /// <summary>
+        /// Public ctor
+        /// </summary>
+        public KNetCLICore()
+        {
+            foreach (var item in ImportList)
+            {
+                ImportPackage(item);
+            }
+        }
+
         protected override string DefaultLog4JConfiguration()
         {
             return Path.Combine(Const.DefaultConfigurationPath, "tools-log4j.properties");
+        }
+
+        public override IEnumerable<IArgumentMetadata> CommandLineArguments
+        {
+            get
+            {
+                var lst = new List<IArgumentMetadata>(base.CommandLineArguments);
+                lst.AddRange(new IArgumentMetadata[]
+                {
+                    new ArgumentMetadata<string>()
+                    {
+                        Name = CLIParam.Interactive[0],
+                        ShortName = CLIParam.Interactive[1],
+                        Type = ArgumentType.Single,
+                        Help = "Activate an interactive shell",
+                    },
+                    new ArgumentMetadata<string>()
+                    {
+                        Name = CLIParam.NoLogo[0],
+                        ShortName = CLIParam.NoLogo[1],
+                        Type = ArgumentType.Single,
+                        Help = "Do not display initial informative string",
+                    },
+                    new ArgumentMetadata<string>()
+                    {
+                        Name = CLIParam.Script[0],
+                        ShortName = CLIParam.Script[1],
+                        Type = ArgumentType.Double,
+                        Help = "Run the script code and exit, the argument is the path to the file containing the script",
+                    },
+                    new ArgumentMetadata<string>()
+                    {
+                        Name = CLIParam.JarList[0],
+                        ShortName = CLIParam.JarList[1],
+                        Type = ArgumentType.Double,
+                        Help = "A CSV list of JAR to be used or folders containing the JARs",
+                    },
+                    new ArgumentMetadata<string>()
+                    {
+                        Name = CLIParam.NamespaceList[0],
+                        ShortName = CLIParam.NamespaceList[1],
+                        Type = ArgumentType.Double,
+                        Help = "A CSV list of namespace to be used for interactive shell, JNet/KNet namespaces are added automatically",
+                    },
+                    new ArgumentMetadata<string>()
+                    {
+                        Name = CLIParam.ImportList[0],
+                        ShortName = CLIParam.ImportList[1],
+                        Type = ArgumentType.Double,
+                        Help = "A CSV list of import to be used",
+                    },
+                });
+                return lst;
+            }
         }
 
         protected override string[] ProcessCommandLine()
         {
             var result = base.ProcessCommandLine();
 
-            if (!string.IsNullOrEmpty(ClassToRun))
-            {
-                Type type = null;
+            _Interactive = ParsedArgs.Exist(CLIParam.Interactive[0]);
+            _NoLogo = ParsedArgs.Exist(CLIParam.NoLogo[0]);
 
-                foreach (var item in typeof(KNetCore<>).Assembly.ExportedTypes)
-                {
-                    if (item.Name == ClassToRun || item.FullName == ClassToRun)
-                    {
-                        type = item;
-                        break;
-                    }
-                }
-                MainClassToRun = type ?? throw new ArgumentException($"Requested class {ClassToRun} is not a valid class name.");
+            _Script = ParsedArgs.Get<string>(CLIParam.Script[0]);
+
+            List<string> jarList = new List<string>();
+            if (ParsedArgs.Exist(CLIParam.JarList[0]))
+            {
+                var jars = ParsedArgs.Get<string>(CLIParam.JarList[0]).Split(',', ';');
+                jarList.AddRange(jars);
             }
+            _JarList = jarList;
+
+            List<string> namespaceList = new List<string>();
+
+            var jnetAssembly = typeof(JNetCoreBase<>).Assembly;
+            foreach (var item in jnetAssembly.GetExportedTypes())
+            {
+                if (item.IsPublic)
+                {
+                    if (!namespaceList.Contains(item.Namespace)) namespaceList.Add(item.Namespace);
+                }
+            }
+            if (ParsedArgs.Exist(CLIParam.NamespaceList[0]))
+            {
+                var namespaces = ParsedArgs.Get<string>(CLIParam.JarList[0]).Split(',', ';');
+                foreach (var item in namespaces)
+                {
+                    if (!namespaceList.Contains(item)) namespaceList.Add(item);
+                }
+            }
+            _NamespaceList = namespaceList;
+
+            List<string> importList = new List<string>();
+            if (ParsedArgs.Exist(CLIParam.ImportList[0]))
+            {
+                var imports = ParsedArgs.Get<string>(CLIParam.JarList[0]).Split(',', ';');
+                foreach (var item in imports)
+                {
+                    if (!importList.Contains(item)) importList.Add(item);
+                }
+            }
+            _ImportList = importList;
+
+            PrepareMainClassToRun(ClassToRun);
 
             switch (ClassToRun)
             {
@@ -107,6 +234,20 @@ namespace MASES.KNetCLI
             }
 
             return result;
+        }
+
+        /// <inheritdoc cref="JNetCoreBase{T}.PathToParse"/>
+        protected override IList<string> PathToParse
+        {
+            get
+            {
+                var lst = base.PathToParse;
+                foreach (var item in _JarList)
+                {
+                    lst.Add(Path.GetFullPath(item));
+                }
+                return lst;
+            }
         }
     }
 
