ad4c56d378621bd585718de5c00f1b264924fdb5
masesdevelopers <94312179+masesdevelopers@users.noreply.github.com>
2024-06-05 18:42:31 +0200

    Update wrong name in script

--- src/container/KNetTestRun.sh
+++ src/container/KNetTestRun.sh
@@ -1,12 +1,10 @@
 #!/bin/bash
 
-ls -las /app
-
 # Start zookeeper
-dotnet /app/KNetCLI.dll -ClassToRun ZooKeeperStart /app/config/zookeeper.config &
+dotnet /app/MASES.KNetCLI.dll -ClassToRun ZooKeeperStart /app/config/zookeeper.config &
 
 # Start kafka broker
-dotnet /app/KNetCLI.dll -ClassToRun KafkaStart /app/config/zookeeper.config &
+dotnet /app/MASES.KNetCLI.dll -ClassToRun KafkaStart /app/config/zookeeper.config &
 
 # Wait for any process to exit
 wait -n
