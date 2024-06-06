8e8c5a977349363af13d2d2678a1cacf0b25c89c
masesdevelopers <94312179+masesdevelopers@users.noreply.github.com>
2024-06-06 03:18:26 +0200

    Tested locally, verify online

--- src/container/KNetTestRun.sh
+++ src/container/KNetTestRun.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 # Start zookeeper
-dotnet /app/MASES.KNetCLI.dll -ClassToRun ZooKeeperStart /app/zookeeper.properties &
+dotnet /app/MASES.KNetCLI.dll -ClassToRun ZooKeeperStart -Log4JConfiguration /app/config/log4j.properties /app/zookeeper.properties &
 
 # Start kafka broker
-dotnet /app/MASES.KNetCLI.dll -ClassToRun KafkaStart /app/server.properties &
+dotnet /app/MASES.KNetCLI.dll -ClassToRun KafkaStart -Log4JConfiguration /app/config/log4j.properties /app/server.properties &
 
 # Wait for any process to exit
 wait -n
