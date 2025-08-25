31960ebf0e7cbc6422b4d4c9615bd4c193fade5f
masesdevelopers <94312179+masesdevelopers@users.noreply.github.com>
2025-08-25 22:56:45 +0200

    KNetProducer returns Future<RecordMetadata> like other methods even if the record is built on JVM side

--- src/jvm/knet/src/main/java/org/mases/knet/developed/clients/producer/KNetProducer.java
+++ src/jvm/knet/src/main/java/org/mases/knet/developed/clients/producer/KNetProducer.java
@@ -21,11 +21,13 @@ package org.mases.knet.developed.clients.producer;
 import org.apache.kafka.clients.producer.Callback;
 import org.apache.kafka.clients.producer.KafkaProducer;
 import org.apache.kafka.clients.producer.ProducerRecord;
+import org.apache.kafka.clients.producer.RecordMetadata;
 import org.apache.kafka.common.header.Header;
 import org.apache.kafka.common.serialization.Serializer;
 
 import java.util.Map;
 import java.util.Properties;
+import java.util.concurrent.Future;
 
 public class KNetProducer<K, V> extends KafkaProducer<byte[], byte[]> {
     Callback _callback = null;
@@ -50,34 +52,33 @@ public class KNetProducer<K, V> extends KafkaProducer<byte[], byte[]> {
         _callback = callback;
     }
 
-    public void send(String topic, Integer partition, Long timestamp, byte[] key, byte[] value, Iterable<Header> headers) {
+    public Future<RecordMetadata> send(String topic, Integer partition, Long timestamp, byte[] key, byte[] value, Iterable<Header> headers) {
         ProducerRecord record = new ProducerRecord(topic, partition, timestamp, key, value, headers);
-        send(record, _callback);
+        return send(record, _callback);
     }
 
-    public void send(String topic, Integer partition, Long timestamp, byte[] key, byte[] value) {
+    public Future<RecordMetadata> send(String topic, Integer partition, Long timestamp, byte[] key, byte[] value) {
         ProducerRecord record = new ProducerRecord(topic, partition, timestamp, key, value);
-        send(record, _callback);
+        return send(record, _callback);
     }
 
-    public void send(String topic, Integer partition, byte[] key, byte[] value, Iterable<Header> headers) {
+    public Future<RecordMetadata> send(String topic, Integer partition, byte[] key, byte[] value, Iterable<Header> headers) {
         ProducerRecord record = new ProducerRecord(topic, partition, key, value, headers);
-        send(record, _callback);
+        return send(record, _callback);
     }
 
-    public void send(String topic, Integer partition, byte[] key, byte[] value) {
+    public Future<RecordMetadata> send(String topic, Integer partition, byte[] key, byte[] value) {
         ProducerRecord record = new ProducerRecord(topic, partition, key, value);
-        send(record, _callback);
+        return send(record, _callback);
     }
 
-    public void send(String topic, byte[] key, byte[] value) {
+    public Future<RecordMetadata> send(String topic, byte[] key, byte[] value) {
         ProducerRecord record = new ProducerRecord(topic, key, value);
-        send(record, _callback);
+        return send(record, _callback);
     }
 
-    public void send(String topic, byte[] value) {
+    public Future<RecordMetadata> send(String topic, byte[] value) {
         ProducerRecord record = new ProducerRecord(topic, value);
-        send(record, _callback);
+        return send(record, _callback);
     }
-
 }
