69c938756e464f778d3e811e651e2bae68ce5683
masesdevelopers <94312179+masesdevelopers@users.noreply.github.com>
2025-09-21 05:12:05 +0200

    Alignment

--- src/net/KNet/Developed/Org/Apache/Kafka/Streams/Kstream/NamedOperation.cs
+++ src/net/KNet/Developed/Org/Apache/Kafka/Streams/Kstream/NamedOperation.cs
@@ -41,6 +41,11 @@ namespace Org.Apache.Kafka.Streams.Kstream
         /// </summary>
         public NamedOperation() { }
         /// <summary>
+        /// Internal constructor: used internally from JCOBridge
+        /// </summary>
+        [global::System.Obsolete("This public initializer is needed for JCOBridge internal use, other uses can produce unidentible behaviors.")]
+        public NamedOperation(IJVMBridgeBaseInitializer initializer) : base(initializer) { }
+        /// <summary>
         /// Generic constructor: it is useful for JCOBridge when there is a derived class which needs to pass arguments to the highest JVMBridgeBase class
         /// </summary>
         public NamedOperation(params object[] args) : base(args) { }
