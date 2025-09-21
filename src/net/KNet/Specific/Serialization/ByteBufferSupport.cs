643c497d68accb1472837c9dd401745ea501f768
masesdevelopers <94312179+masesdevelopers@users.noreply.github.com>
2025-09-21 16:25:41 +0200

    Class fix

--- src/net/KNet/Specific/Serialization/ByteBufferSupport.cs
+++ src/net/KNet/Specific/Serialization/ByteBufferSupport.cs
@@ -94,6 +94,11 @@ namespace MASES.KNet.Serialization
             /// </summary>
             public ByteBufferSerde() { }
             /// <summary>
+            /// Internal constructor: used internally from JCOBridge
+            /// </summary>
+            [global::System.Obsolete("This public initializer is needed for JCOBridge internal use, other uses can produce unidentible behaviors.")]
+            public ByteBufferSerde(IJVMBridgeBaseInitializer initializer) : base(initializer) { }
+            /// <summary>
             /// Generic constructor: it is useful for JCOBridge when there is a derived class which needs to pass arguments to the highest JVMBridgeBase class
             /// </summary>
             public ByteBufferSerde(params object[] args) : base(args) { }
