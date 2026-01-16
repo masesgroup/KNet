5ef295b1a21810bb8d81b81d756f2c83645fe591
masesdevelopers <94312179+masesdevelopers@users.noreply.github.com>
2026-01-16 20:37:57 +0100

    Test cleanup

--- tests/net/Connect/KNetConnectTest/KNetConnectSource.cs
+++ tests/net/Connect/KNetConnectTest/KNetConnectSource.cs
@@ -88,7 +88,7 @@ namespace MASES.KNet.Connect.Test
             {
                 if (useSourceOffset)
                 {
-                    var record = CreateRecord(PartitionOrOffsetForKey((Java.Lang.String)FILENAME_FIELD, _filename), PartitionOrOffsetForKey((Java.Lang.String)POSITION_FIELD, i), _topic, Schema.STRING_SCHEMA, lines[i], DateTime.Now);
+                    var record = CreateRecord(PartitionOrOffsetForKey(FILENAME_FIELD, _filename), PartitionOrOffsetForKey(POSITION_FIELD, i), _topic, Schema.STRING_SCHEMA, lines[i], DateTime.Now);
                     records.Add(record);
                 }
                 else
