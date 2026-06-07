3dfe9558fd319bea7bd2f48b4993aaa12aa13b5e
masesdevelopers <94312179+masesdevelopers@users.noreply.github.com>
2026-06-07 20:03:54 +0200

    Return null in case the iterator returns null

--- src/net/KNet/Specific/Streams/State/WindowedKeyValueIterator.cs
+++ src/net/KNet/Specific/Streams/State/WindowedKeyValueIterator.cs
@@ -163,6 +163,7 @@ namespace MASES.KNet.Streams.State
             IGenericSerDesFactory factory = Factory;
             _valueSerDes ??= factory?.BuildValueSerDes<V, TJVMV>();
             var kv = _iterator.Next();
+            if (kv == null) return null;
             return new WindowedKeyValue<K, V, TJVMK, TJVMV>(factory, KeyValueSupport<Org.Apache.Kafka.Streams.Kstream.Windowed<TJVMK>, TJVMV>.Create(kv), _valueSerDes, false);
         }
         /// <summary>
